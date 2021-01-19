db3b4990393f2241324764aa4d6244f88647e2fd
adventure-star <Boristolstyakov@protonmail.com>
2021-01-19 09:00:34 +0700

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <input
+      class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+      type="email"
+      placeholder="jane@example.com"
+    />
+    <p class="text-large text-red-500 hover:text-blue-500">Blue Text</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped></style>
